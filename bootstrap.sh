671fd7bca20a8d6a7d50a101f87a4ced9c1e0277
andrea-v.corrado@db.com <andrea-v.corrado@db.com>
2023-03-11 19:08:51 +0100

    sparse checkout command script

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,26 +6,36 @@ die() {
 	exit 1
 }
 
-TEAM=$1
+options=(
+    '{"name": "client/android", "folders": "client/android"}'
+    '{"name": "service/common service/identity", "folders": "service/common service/identity"}'
+    '{"name": "service", "folders": "service"}'
+    '{"name": "web/browser", "folders": "web/browser"}'
+)
 
-case $TEAM in
-"android")
-	FOLDERS="client/android"
-	;;
-"identity")
-	FOLDERS="service/common service/identity"
-	;;
-"browser")
-	FOLDERS="service web/browser"
-	;;
-*)
-	die "please specify a valid team"
-	;;
-esac
+declare -A optionMap=()
+for ((i=0; i<${#options[@]}; i++)); do
+    name=$(echo "${options[i]}" | grep -o "\"name\": \"[^\"]*\"" | cut -d'"' -f4)
+    optionMap["$((i+1))"]="${options[i]}"
+    echo "$((i+1))) $name"
+done
 
-echo "Running 'git sparse-checkout init --cone'"
+while true; do
+    read -n1 -rp $'\nSelect a folder to sparse checkout: ' opt
+    echo
+    if [[ -z "${optionMap[$opt]}" ]]; then
+        echo "Invalid option"
+    else
+        break
+    fi
+done
+
+FOLDERS=$(echo "${optionMap[$opt]}" | grep -o "\"folders\": \"[^\"]*\"" | cut -d'"' -f4)
+
+echo "Initializing sparse checkout"
 git sparse-checkout init --cone
 
-echo "Running 'git sparse-checkout set $FOLDERS'"
+echo "Setting sparse checkout to: $FOLDERS"
 git sparse-checkout set $FOLDERS
 
+echo "Sparse checkout complete"
